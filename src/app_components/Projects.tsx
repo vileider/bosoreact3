f8f589e031167990549202345dfecbbac97aaac6
vileider <andrewbagio@yahoo.com>
2022-07-11 20:33:36 +0100

    tracing mouse on project div
    Co-authored-by: szymonbasiul <szymonbasiul@users.noreply.github.com>

--- src/app_components/Projects.tsx
+++ src/app_components/Projects.tsx
@@ -2,18 +2,31 @@ import "./styles/Projects.css";
 import "../reusable_styles/BackgroundGlow.css";
 
 export function Projects() {
-	const body = (
-		<div className="projectBody">
-			<div className="projectTitle">Our Projects</div>
-			<div className="projectsSlider">
-				<div className="arrow left "></div>
-				<div className="project backgroundGlow"> 1st</div>
-				<div className="project backgroundGlow"> 2nd</div>
-				<div className="project backgroundGlow"> 3rd</div>
-				<div className="arrow right"></div>
-			</div>
-		</div>
-	);
+  const mouseTraceLog = (x: any) => {
+    console.log(x.clientX - x.target.offsetLeft);
+  };
 
-	return body;
+  const body = (
+    <div className="projectBody">
+      <div className="projectTitle">Our Projects</div>
+      <div className="projectsSlider">
+        {/* <div className="arrow left "></div> */}
+        <div
+          className="project backgroundGlow"
+          onClick={(event: any) => mouseTraceLog(event)}
+          onDrag={(event: any) => {
+            event.target.clientWidth = event.clientX - event.target.offsetLeft;
+          }}
+        >
+          {" "}
+          1st
+        </div>
+        <div className="project backgroundGlow"> 2nd</div>
+        <div className="project backgroundGlow"> 3rd</div>
+        {/* <div className="arrow right"></div> */}
+      </div>
+    </div>
+  );
+
+  return body;
 }
